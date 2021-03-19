f9f6635cb2617df76b2ffb3cca2f3151414c6d7a
leisyarcia <leisyarcia@gmail.com>
2021-03-19 03:10:50 +0000

    done

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,6 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
 import { Home } from "./component/home.js";
 
 //render your react application
